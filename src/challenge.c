edf1d544282eb627632c067b9dd32deb8ba5f972
Lucien GHEERBRANT <lucien.gheerbrant@etu.emse.fr>
2023-06-12 13:09:20 +0200

    feat: Add algo1

---
+++ src/challenge.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "environnement.h"
+
+#include "challenge.h"
+
+void algo1(environnement_st *env) {
+    for (int i = 0; i < env->l; i++) {
+        for (int j = 0; j < env->c; j++){
+            if (env->grille[i][j].etat == CIBLE) {
+                env->grille[i][j].etat = CIBLE_ET_SURVEILLANT;
+            }
+        }
+    }
+}
